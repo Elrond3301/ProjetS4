4e7729a26904a3af65f496c366d860bd20720c38
Elrond3301 <67201386+Elrond3301@users.noreply.github.com>
2021-04-11 16:54:32 +0200

    Update Block.java

--- Block.java
+++ Block.java
@@ -1,73 +1,67 @@
-package block;
-
-import java.security.MessageDigest;
-import java.text.DateFormat;
+package ProjetS4.src.block;
+import java.util.Date;
+import java.util.ArrayList;
 
 /**
  * Classe Block qui a pour attribut un indice, une date, une liste de transactions, un hashCode, le hashcode du block 
- * précédent et une nonce initialisé à0
+ * precedent et une nonce initialise a 0
  *
  * @author Simon Hautesserres
- * @date 09/03/2021
+ * @date 11/04/2021
  * @version 1.0
  */
 
 public class Block {
 	private int index;
 	private int nonce = 0;
-	private String transaction;
-	private DateFormat timeStamp;
+	private ArrayList<String> transaction; /*A mettre dans Objet Transaction*/
+	private int maxTransactions;
+	private String hashTransaction= "";
+	private int difficulty;
+	/*Fin objet Transaction*/
+	private Date timeStamp;
 	private String hashCode;
-	private String previousHash;
+	private String previousHash = "0";
+	MerkleTree mkTree = new MerkleTree();
 	
 	/** 
-	 * Constructeur qui met à jour la veleur de l'index, de la date, des transactions et du hash précédent
+	 * Constructeur qui met a jour la valeur de l'index, de la date, crée le merkle root des transaction  et du hash precedent
 	 * @param index
 	 * @param timeStamp
 	 * @param transaction
 	 * @param previousHash
      */
-	public Block(int index, DateFormat timeStamp, String previousHash) {
+	public Block(int index, Date timeStamp, String previousHash, int difficulty, int maxTransactions) {
 		this.index = index;
 		this.timeStamp = timeStamp;
-		this.previousHash = previousHash;
-	    calculateHash();
+		this.transaction = new ArrayList<String>(maxTransactions);
+		this.maxTransactions = maxTransactions;
+		this.difficulty = difficulty;
+		calculateHash(); /*On calcule une premiere fois le Hash*/
+		/* Si le block n'est pas le premier cree*/
+		if(index!=0) {
+			mining(difficulty); /*On mine en fonction de la difficulté le bloc*/
+			this.previousHash = previousHash; /*On actualise le hash du bloc précédent*/
+		}
+		else {
+			transaction.add("Genesis"); /*Le bloc contient uniquement la transaction "Genesis"*/
+			hashTransaction = mkTree.createMerkleTree(transaction);
+			nonce--; /*Il a une nonce nulle*/
+		}
+	    
 	}
 	
 	/** 
-	 * Méthode qui appelle la méthode sha256, incrémente la nonce et retourne le hashCode
+	 * Methode qui appelle la methode sha256, incremente la nonce et actualise le hashCode
 	 * @return hashCode
 	 */
 	private void calculateHash() {
-		this.hashCode = applySha256(this.index+this.timeStamp.toString()+this.transaction+this.previousHash+this.nonce);
-		nonce++;
+		HashUtil hash = new HashUtil(); /*On crée un hash pour ensuite le calculer et l'actualiser dans hashCode*/
+		this.hashCode = hash.applySha256(this.index+this.timeStamp.toString()+this.hashTransaction+this.previousHash+this.nonce);
+		this.nonce++;
 	}
 	
-	/** 
-	 * Méthode qui prend en entrée un String et utilise la méthode du sha256 pour retourner le hashCode
-	 * @param input
-	 * @return hashCode
-	 */
-	public String applySha256(String input){
-		  try {
-		   MessageDigest digest = MessageDigest.getInstance("SHA-256");
-		         
-		   //Applies sha256 to our input, 
-		   byte[] hash = digest.digest(input.getBytes("UTF-8"));
-		         
-		   StringBuffer hexString = new StringBuffer(); // This will contain hash as hexidecimal
-		   for (int i = 0; i < hash.length; i++) {
-		    String hex = Integer.toHexString(0xff & hash[i]);
-		    if(hex.length() == 1) hexString.append('0');
-		    hexString.append(hex);
-		   }
-		   return hexString.toString();
-		  }
-		  catch(Exception e) {
-		   throw new RuntimeException(e);
-		  }
-		 }
-
+	
 	/** 
 	 * Getter qui retourne l'index du block
 	 * @return index
@@ -86,17 +80,18 @@ public class Block {
 	
 	/** 
 	 * Getter qui retourne la liste des transactions
+	 * @param index
 	 * @return transaction
 	 */
-	public String getTransaction() {
-		return transaction;
+	public String getTransaction(int index) {
+		return transaction.get(index);
 	}
 	
 	/** 
-	 * Getter qui retourne la date de création du block
+	 * Getter qui retourne la date de creation du block
 	 * @return timeStamp
 	 */
-	public DateFormat getTimeStamp() {
+	public Date getTimeStamp() {
 		return timeStamp;
 	}
 	
@@ -105,11 +100,19 @@ public class Block {
 	 * @return hashCode
 	 */
 	public String getHashCode() {
-		return hashCode;
+		return this.hashCode;
 	}
 	
 	/** 
-	 * Getter qui retourne le hash du bloc précédent
+	 * Getter qui retourne le hash code des transactions calcules avec le merkle tree
+	 * @return hashTransaction
+	 */
+	public String getHashTransaction() {
+		return this.hashTransaction;
+	}
+	
+	/** 
+	 * Getter qui retourne le hash du bloc precedent
 	 * @return previousHash
 	 */
 	public String getPreviousHash() {
@@ -117,42 +120,54 @@ public class Block {
 	}
 	
 	/** 
-	 * Méthode toString qui retourne une chaine concaténée des attributs du block
+	 * Methode toString qui retourne une chaine concatenee des attributs du block
 	 * @return chaine
 	 */
 	public String toString() {
-		return "Block #" + index + "\n[nonce : " + nonce + "\ntransaction : " + transaction + "\ntimeStamp : "
-				+ timeStamp + "\nhashCode : " + hashCode + "\npreviousHash : " + previousHash + "]\n\n";
+		return "Block Mined !! : "+getHashCode()+" n° : "+getIndex()+" Nonce = "+getNonce()+" Previous Hash: "+getPreviousHash()+" Transaction: "+getHashTransaction()+"\n";
 	}
 	
 	/**
-	 * Méthode qui vérifie si le hashCode respecte la difficulté qui correspond à difficulté * "0" au début du hash
+	 * Methode qui verifie si le hashCode respecte la difficulte qui correspond Ã  difficulte * "0" au debut du hash
 	 * @param difficulty
 	 * @return boolean
 	 */
 	public boolean isHashCodeValid(int difficulty) {
-		String zeros = zeros(difficulty);
-        for(int i = 0; i<difficulty;i++)
-             if (!getHashCode().substring(0, difficulty).equals(zeros)) {
+		String zeros = zeros(difficulty); /*On initialise avec un nombre definis de zeros par difficulte*/
+        if (!getHashCode().substring(0, difficulty).equals(zeros)) { /*Si le début du hash code n'a pas le bon nombre de zeros, on retourne false*/
                  return false;
-            }
+        }
         return true;
 }
 
 	/**
-	 * Méthode qui vérifie si le hashCode est valide et le recalcule tant que celui ci ne respecte pas la difficulté
+	 * Methode qui verifie si le hashCode est valide et le recalcule tant que celui ci ne respecte pas la difficulte
 	 * @param difficulty
-	 * @return nonce
 	 */
-    public int mining(int difficulty){
+    public void mining(int difficulty){
         while(!isHashCodeValid(difficulty)) {
             calculateHash();
         }
-        return nonce;
     } 
     
     /**
-     * Méthode qui renvoie la prime qui décroit de moitié tous les N blocs
+     * Methode qui concatente des zeros "difficulty" fois dans un String
+     * @param difficulty
+     * @return zeros
+     */
+    public String zeros(int difficulty) {
+    	String zeros = "";
+    	for(int i = 0; i< difficulty; i++) {
+    		zeros+="0";
+    	}
+    	return zeros;
+    }
+    
+    /*== PARTIE TRANSACTIONS A FAIRE DANS UN AUTRE OBJET == */
+    
+    
+    /**
+     * Methode qui renvoie la prime qui decroit de moitie tous les N blocs
      * @param nbBlocks
      * @return prime
      */
@@ -173,18 +188,12 @@ public class Block {
 		}
     }
     
-    /**
-     * Méthode qui concatènte des zéros "difficulty" fois dans un String
-     * @param difficulty
-     * @return zeros
-     */
-    public String zeros(int difficulty) {
-    	String zeros = "";
-    	for(int i = 0; i< difficulty; i++) {
-    		zeros+="0";
-    	}
-    	return zeros;
+    public void addTransaction(String trans) {
+    	if(transaction.size() == maxTransactions)
+    		return;
+    	transaction.add(trans);
+    	hashTransaction = mkTree.createMerkleTree(transaction);
+    	calculateHash();
+    	mining(difficulty);
     }
-	
-	
 }
