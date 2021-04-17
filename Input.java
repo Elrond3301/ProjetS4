5f128e992db9c1f039279c82331507cabb9e2ff7
Elrond3301 <67201386+Elrond3301@users.noreply.github.com>
2021-04-17 14:27:40 +0200

    Update Input.java

--- Input.java
+++ Input.java
@@ -1,15 +1,29 @@
 package ProjetS4.src.block;
 
 import java.util.Scanner;
-
+/**
+ * Classe Input qui contient des fonctions utiles pour la saisie d'entier ou de chaine avec un seul attribut Scanner
+ *
+ * @author Simon Hautesserres
+ * @date 17/04/2021
+ * @version 1.0
+ */
 public class Input {
-
 	private Scanner scanner = new Scanner(System.in);
+	
+	/** 
+	 * Methode qui utilise le scanner pour renvoyer une chaine de caractères de l'entrée standard
+	 * @return string
+	 */
 	public String inputString() {
 		String string = scanner.nextLine();
 		return string;
 	}
 	
+	/** 
+	 * Methode qui utilise le scanner pour renvoyer un int de l'entrée standard
+	 * @return number
+	 */
 	public int inputInt() {
 	    int number = scanner.nextInt();
 		return number;
