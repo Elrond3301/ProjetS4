c7ed59392947d7b9ba977f176744013fe8179d73
Elrond3301 <67201386+Elrond3301@users.noreply.github.com>
2021-04-26 09:51:10 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 package ProjetS4.src.block;
-
+import java.util.Queue;
+import java.util.LinkedList;
 /**
  * Classe Test qui lance la création de la blockchain, demande à l'utilisateur les différents parametres, lit et écrit grâce à JSON
  * et affiche différents éléments dont la blockchain grâce à la fonction affichage
@@ -15,7 +16,8 @@ public class Test {
 	 */
 	public static void affichage(Blockchain blockchain, int delay) {
 		System.out.println("======== GENESIS ======== \n"+ blockchain.getBlocks(0).toString()+"\n=== PHASE HELICOPTERE ===");
-		for(int k = 1; k < blockchain.getMaxBlocks(); k++) {
+		int k = 1;
+		while(k < blockchain.getMaxBlocks() && blockchain.getBlocks(k) != null) {
 			try {
 	            Thread.sleep(delay);
 	        }
@@ -23,6 +25,7 @@ public class Test {
 	            e.printStackTrace();
 	        }
 			System.out.println(blockchain.getBlocks(k));
+			k++;
 		}
 	}
 	
@@ -30,24 +33,30 @@ public class Test {
 	
 	public static void main(String[] args) {
 		Input input = new Input();
-		/*On demande la difficulte, le nombre de blocs et le nombre de transactions max en entree a  l'utilisateur*/
+		Queue<Integer> q = new LinkedList<>(); /*File globale de transactions*/
+		int reward = 50; /*Reward initial à 50 bonobos*/
+		/*On demande  le nombre de blocs, le nombre d'utilisateurs, la difficulte, et le nombre de transactions max en entree a  l'utilisateur*/
 		System.out.println(" == INPUT == \n-- Rentrez le nombre de blocs max --\nChoix:  ");
 		int nbBlocks = input.inputInt();
+		System.out.println("-- Rentrez le nombre d'utilisateurs --\nChoix:  ");
+		int nbUtilisateurs = input.inputInt();
 		System.out.println("-- Rentrez la difficulté du hash --\nChoix: ");
 		int difficulty = input.inputInt();
 		System.out.println("-- Rentrez le nombre max de transactions --\nChoix: ");
 		int maxTransactions = input.inputInt();
 		
-		/*On lance la creation d'une blockchain avec un nombre de blocs etablis*/
+		/*On lance la creation d'une blockchain et des mineurs*/
+		Mineur [] mineurs = new Mineur[nbUtilisateurs];
+		mineurs[0] = new Mineur("Creator",reward);
 		Blockchain blockchain = new Blockchain(nbBlocks, difficulty, maxTransactions);
-		for(int k = 1; k < nbBlocks; k++) {
-			blockchain.addBlock();
+		for(int k = 1; k < nbUtilisateurs; k++) {
+			mineurs[k] = new Mineur("Usern"+k,reward);
+			blockchain.addBlock(mineurs[0].getNom()+" envoie "+reward+" Bnb à "+ mineurs[k].getNom()); /*Phase Helicopter Money où chaque User reçoit 50 bonobos*/
 		}
+		/*Phase de marché*/
+		
+		
 		affichage(blockchain, 1000);
-		/*On ajoute un bloc et des transactions*/
-		blockchain.addBlock();
-		blockchain.getBlocks(1).getTransaction().addTransaction("Michel a 10 bonobos");
-		blockchain.isBlockchainValid();
 		/*Ecriture de la blockchain dans un fichier texte*/
 		BCJsonUtils.BCJsonWriter(blockchain, "C:\\Users\\chris\\OneDrive\\Bureau\\UPS\\ProjetS4\\test.txt");
 	}
